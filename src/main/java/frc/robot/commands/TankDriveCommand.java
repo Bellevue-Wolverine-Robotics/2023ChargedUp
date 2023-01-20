8bf3b11fd9ba890415337955cabbad62c20b5c6e
enyoukai <an.josephZero@gmail.com>
2023-01-19 18:56:37 -0800

    tank drive cleaning

--- src/main/java/frc/robot/commands/TankDriveCommand.java
+++ src/main/java/frc/robot/commands/TankDriveCommand.java
@@ -3,11 +3,13 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Istream.IStreamBundle;
 import frc.robot.subsystems.DriveSubsystem;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
 
 public class TankDriveCommand extends CommandBase {
     private DriveSubsystem driveSubsystem;
     private IStreamBundle input;
-
+    /*byte shoulderMotorChannel;
+    Spark shoulder = new Spark(shoulderMotorChannel);*/
     public TankDriveCommand(DriveSubsystem driveSubsystem, IStreamBundle input){
         this.driveSubsystem = driveSubsystem;
         this.input = input;
@@ -16,7 +18,7 @@ public class TankDriveCommand extends CommandBase {
 
     @Override
     public void execute(){
-
+        // shoulder.set()
         
         driveSubsystem.tankDrive(input.getY(1), input.getY(2));
     }
