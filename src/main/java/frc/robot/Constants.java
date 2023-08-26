437ec55926f2d10e2c504463530e6488350df76b
Justin Jiang <justin.jiang.co@gmail.com>
2023-08-26 11:45:15 -0700

    clean up

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -37,8 +37,6 @@ public final class Constants {
       public static final int RIGHT_FRONT = 3;
       public static final int RIGHT_BACK = 4;
       public static final int ARM_SPARKMAX = 5;
-      //public static final int ARM_TALON = 6;
-
       public static final int ARM_NEO_MOTOR = 5;
   }
 
